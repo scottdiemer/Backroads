c93b42233fa5c0eabe3e276316ec8f20db51ac2d
scottdiemer <sdiemer@cfl.rr.com>
2019-05-24 16:34:17 -0400

    played around with css modules

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <div>
       <h1>I'm footer</h1>
+      <div className="links">i'm footer link</div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react"
+// import "./navbar.css"
+import styles from "./navbar.module.css"
+console.log(styles)
 
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.navbar}>
       <h1>I'm navbar</h1>
+      <div className={styles.links}>i'm navbar link</div>
     </div>
   )
 }
