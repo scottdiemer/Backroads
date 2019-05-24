bb2a61012ed81304eac8ab6663e7c05f6a9ebd2c
scottdiemer <sdiemer@cfl.rr.com>
2019-05-23 21:46:15 -0400

    created layout and simple navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    Hello world!
+    <Link to="/blog/">blog page</Link>
+    <h1 style={{ fontSize: "20px", textTransform: "capitalize", color: "red" }}>
+      hello styling
+    </h1>
+  </Layout>
+)
