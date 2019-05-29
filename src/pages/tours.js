e58536cfde7933ce4e2ce54e0301c32e539dd130
scottdiemer <sdiemer@cfl.rr.com>
2019-05-28 22:47:36 -0400

    toying around with styled-components

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
+import Button from "../examples/Button"
 
 export default class tours extends Component {
   render() {
-    return <Layout>hello from tours page!!!!</Layout>
+    return (
+      <Layout>
+        hello from tours page!!!!
+        <div>
+          <Button big>first button</Button>
+          <Button color="#f15025">second button</Button>
+        </div>
+      </Layout>
+    )
   }
 }
